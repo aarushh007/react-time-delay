53a17c8709e606072304f0d11b972b04142f1d11
aarushh007 <70344024+aarushh007@users.noreply.github.com>
2021-01-31 12:44:42 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const [value, setValue] = React.useState(0);
+  const [delay, setDelay] = React.useState(2000);
+  const changeDelay = () => {
+    let input = document.getElementById("delay");
+    input = parseInt(input.value);
+    setDelay(input * 1000);
+    document.getElementById("delay").value = "";
+  };
+  const increase = () => {
+    setTimeout(() => {
+      setValue((prevState) => {});
+    }, delay);
+  };
+  return (
+    <div className="App">
+      <h1>{value}</h1>
+      <button onClick={increase}>Increase with delay</button>
+      <br />
+      <br />
+      <input id="delay" placeholder="enter delay amount" />
+      <button onClick={changeDelay}>Submit</button>
+      <br />
+      <br />
+      <h3>current delay: {delay / 1000} seconds</h3>
+    </div>
+  );
+}
